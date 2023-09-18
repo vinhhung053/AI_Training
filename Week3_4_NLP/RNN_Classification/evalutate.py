f3e127440b9dee1dd05d533dde6cd4e9cf61a23d
LAP13385 <vinhhung053@gmail.com>
2023-09-18 13:51:06 +0700

    add os + down model

--- Week3_4_NLP/RNN_Classification/evalutate.py
+++ Week3_4_NLP/RNN_Classification/evalutate.py
@@ -1,32 +1,32 @@
-import numpy as np
-
-
-def binary_cross_entropy(y_predict, y):
-
-    y = np.array(y).reshape(-1, 1)
-    y_predict = np.array(y_predict)
-    score = -1 / (y.shape[0]) * np.sum(y * np.log(y_predict) + (1 - y) * np.log(1 - y_predict))
-    print("  Cross entropy: ", score)
-
-def positive_negative(y_predict, y):
-    y = np.array(y).reshape(-1, 1)
-    y_predict = np.array(y_predict)
-    true_pos = 0
-    true_neg = 0
-    for i in range(y_predict.shape[0]):
-        y_predict[i][0] = (y_predict[i][0] >= 0.5)
-        if(y_predict[i][0] + y[i][0] == 2):
-            true_pos = true_pos + 1
-        elif (y_predict[i][0] + y[i][0] == 0):
-            true_neg = true_neg + 1
-    print("  true_pos / number sample: ", true_pos,"/",y.shape[0])
-    print("  true_neg / number sample: ", true_neg,"/",y.shape[0])
-    print(sum(y_predict == y)/y.shape[0])
-
-def evaluate_model(x, y, model,type_evaluate):
-    y_predict = model.predict(x)
-
-    if(type_evaluate == "binary_cross_entropy"):
-        binary_cross_entropy(y_predict, y)
-    elif(type_evaluate == "positive_negative"):
-        positive_negative(y_predict, y)
+# import numpy as np
+#
+#
+# def binary_cross_entropy(y_predict, y):
+#
+#     y = np.array(y).reshape(-1, 1)
+#     y_predict = np.array(y_predict)
+#     score = -1 / (y.shape[0]) * np.sum(y * np.log(y_predict) + (1 - y) * np.log(1 - y_predict))
+#     print("  Cross entropy: ", score)
+#
+# def positive_negative(y_predict, y):
+#     y = np.array(y).reshape(-1, 1)
+#     y_predict = np.array(y_predict)
+#     true_pos = 0
+#     true_neg = 0
+#     for i in range(y_predict.shape[0]):
+#         y_predict[i][0] = (y_predict[i][0] >= 0.5)
+#         if(y_predict[i][0] + y[i][0] == 2):
+#             true_pos = true_pos + 1
+#         elif (y_predict[i][0] + y[i][0] == 0):
+#             true_neg = true_neg + 1
+#     print("  true_pos / number sample: ", true_pos,"/",y.shape[0])
+#     print("  true_neg / number sample: ", true_neg,"/",y.shape[0])
+#     print(sum(y_predict == y)/y.shape[0])
+#
+# def evaluate_model(x, y, model,type_evaluate):
+#     y_predict = model.predict(x)
+#
+#     if(type_evaluate == "binary_cross_entropy"):
+#         binary_cross_entropy(y_predict, y)
+#     elif(type_evaluate == "positive_negative"):
+#         positive_negative(y_predict, y)
--- Week3_4_NLP/RNN_Classification/train.py
+++ Week3_4_NLP/RNN_Classification/train.py
@@ -3,8 +3,9 @@ from sklearn.model_selection import train_test_split
 from torch.nn.utils.rnn import pad_sequence
 from model import Net
 from word2vec import Word2Vec
-from evalutate import binary_cross_entropy
+# from evalutate import binary_cross_entropy
 
+import os
 import pandas as pd
 import torchtext
 import torch
@@ -50,11 +51,11 @@ def word2vec_data(x, data_file_path):
 
     return x_embeddings
 def split_data(x, y):
-    x_train, x_temp, y_train, y_temp = train_test_split(x, y, test_size=0.4, random_state=40)
-    x_val, x_test, y_val, y_test = train_test_split(x_temp, y_temp, test_size=0.5, random_state=40)
+    x_train, x_temp, y_train, y_temp = train_test_split(x, y, test_size=0.2, random_state=43)
+    x_val, x_test, y_val, y_test = train_test_split(x_temp, y_temp, test_size=0.5, random_state=43)
     return x_train, y_train, x_val, y_val, x_test, y_test
 
-def train(x_train, y_train):
+def train(x_train, y_train, x_test, y_test, path):
     ds_train = torch.utils.data.TensorDataset(x_train, y_train)
     train_loader = torch.utils.data.DataLoader(ds_train, batch_size=128, shuffle=True)
 
@@ -62,10 +63,10 @@ def train(x_train, y_train):
     # test_loader = torch.utils.data.DataLoader(ds_test, batch_size=256, shuffle=True)
     device = torch.device('cpu')
     classifier = Net().to(device)
-    optimizer = optim.Adam(classifier.parameters(), lr=0.005)  # 0.002 dives 85% acc
+    optimizer = optim.Adam(classifier.parameters(), lr=0.001)  # 0.002 dives 85% acc
     criterion = nn.CrossEntropyLoss()
-
-    epoch_bar = tqdm(range(20),
+    best_acc = 0
+    epoch_bar = tqdm(range(40),
                      desc="Training",
                      position=0,
                      total=2)
@@ -87,44 +88,45 @@ def train(x_train, y_train):
             loss.backward()
             optimizer.step()
             acc = (preds.argmax(dim=1) == labels).float().mean().cpu().item()
+        for param_group in optimizer.param_groups:
+            param_group['lr'] *= 0.95
 
-            # if (i + 1) % 50 == 0:
-            #     acc = 0
-            #
-            #     # with torch.no_grad():
-            #     #     for i, (datapoints_, labels_) in enumerate(test_loader):
-            #     #         preds = classifier(datapoints_.to(device))
-            #     #         acc += (preds.argmax(dim=1) == labels_.to(device)).sum().item()
-            #     # acc /= len(x_test)
+        preds_y_train = classifier(x_train)
+        acc_y_train = (preds_y_train.argmax(dim=1) == y_train).float().mean().cpu().item()
+        print("acc train: ", acc_y_train)
 
-        #     batch_bar.set_postfix(loss=loss.item(),
-        #                           accuracy="{:.2f}".format(acc),
-        #                           epoch=epoch)
-        #     batch_bar.update()
-        #
-        # epoch_bar.set_postfix(loss=loss.cpu().item(),
-        #                       accuracy="{:.2f}".format(acc),
-        #                       epoch=epoch)
-        # epoch_bar.update()
-        preds_y_test = classifier(x_train)
-        acc_y_test = (preds_y_test.argmax(dim=1) == y_train).float().mean().cpu().item()
-        print("acc train: ", acc_y_test)
+        preds_y_test = classifier(x_test)
+        acc_y_test = (preds_y_test.argmax(dim=1) == y_test).float().mean().cpu().item()
+
+        if(acc > best_acc):
+            best_acc = acc
+            best_model = ({
+                'epoch': epoch,
+                'model': classifier,
+                'acc': acc_y_train,
+            }, path)
+            torch.save(best_model,'best_model2.pth')
     return classifier
-def val(x_test, y_test, classifier):
-    preds_y_test = classifier(x_test)
+
+def eva(x_test, y_test):
+    best_model_checkpoint = torch.load('best_model2.pth')
+    best_model = best_model_checkpoint[0]['model']
+
+    preds_y_test = best_model(x_test)
     acc_y_test = (preds_y_test.argmax(dim=1) == y_test).float().mean().cpu().item()
     return acc_y_test
 def main():
-    data_file_path = "/home/lap13385/Projects/Zalo_AI_Fresher_Training/Week3_4_NLP/RNN_Classification/source/"
+    data_file_path = current_directory = os.getcwd() + "/source/"
     corpus, y = read_data(data_file_path)
     x_process = pre_data(corpus, data_file_path)
     x_embeddings = word2vec_data(x_process, data_file_path)
     x = torch.tensor(x_embeddings)
     y = torch.tensor(y)
     x_train, y_train, x_val, y_val, x_test, y_test = split_data(x, y)
-    classifier = train(x_train, y_train)
-    print("acc val: ",val(x_val, y_val, classifier))
-    print("acc test: ",val(x_test, y_test, classifier))
+    # train(x_train, y_train, x_val, y_val, data_file_path)
+    print("acc train final: ", eva(x_train, y_train))
+    print("acc val: ", eva(x_val, y_val))
+    print("acc test: ", eva(x_test, y_test))
 
 if __name__ == "__main__":
     main()
